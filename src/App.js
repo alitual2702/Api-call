0fbe141d556236c05b29e2eb57487f7b81760ec5
ALI <127027235+alikk360@users.noreply.github.com>
2024-04-19 13:24:14 +0500

    api admit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+
 function App() {
+  //------------------------------------------------------------
+  const [item, setitem] = useState([])
+  //------------------------------------------------------------
+
+  const [filterChange, setfilterChange] = useState("")
+  //------------------------------------------------------------
+
+  const callingapi = async () => {
+    let getVar = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let respond = await getVar.json();
+    setitem(respond);
+  }
+
+  //-----------------------------------------------------------
+    function handleClicked() {
+      let eachItem = item.filter((val, ind) => {
+        return val.title.includes(filterChange)
+      })
+      setitem(eachItem)
+  
+    }
+
+  //------------------------------------------------------------
+
+  function filChange(event) {
+    setfilterChange(event.target.value)
+  }
+  //------------------------------------------------------------
+
+  useEffect(() => {
+
+    callingapi()
+  }, [filterChange])
+
+  //------------------------------------------------------------
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h2>API Call (Fetch Method , Async Await) with React JS</h2>
+
+      <div className="input-get">
+
+        <input type="text"
+          value={filterChange}
+          onChange={(e) => { filChange(e) }} />
+        <button
+          onClick={() => {
+            handleClicked()
+          }}>
+          Get</button>
+
+      </div>
+      { handleClicked ?  item.map((str, ind) => {
+        return (
+          <div className="nested" key={ind}>
+            <h1>{str.title}</h1>
+            <p>{str.body}</p>
+          </div>
+        )
+      }) : ""
+    }
+
+
     </div>
   );
 }
